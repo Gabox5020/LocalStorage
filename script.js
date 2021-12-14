447a9d2dbae404e60fed07ebe6c5a1e80b4906fd
Gabriel <gaboxroll.2@gmail.com>
2021-12-14 16:38:58 -0300

    tercer ejemplo

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ window.onload = iniciar;
 function iniciar() {
     var btnAgregar = document.getElementById("btnAgregar");
     btnAgregar.addEventListener("click", clickBtnAgregar)
+    mostrarNotas();
 }
 
 function clickBtnAgregar() {
     var txtNota = document.getElementById("txtNota");
-    alert(txtNota.value)
+
+    localStorage.nota = txtNota.value;
+    mostrarNotas();
+}
+
+function mostrarNotas() {
+    var divNotas = document.getElementById("divNotas");
+    divNotas.innerHTML = localStorage.nota;
 }
