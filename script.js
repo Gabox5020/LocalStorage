7cd2cf0bcda26794a4ac48472d5a5d2f99672db4
Gabriel <gaboxroll.2@gmail.com>
2021-12-14 16:42:16 -0300

    comentarios agregados

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 window.onload = iniciar;
 
 function iniciar() {
+    // Boton Agregar escucha el Click
     var btnAgregar = document.getElementById("btnAgregar");
     btnAgregar.addEventListener("click", clickBtnAgregar)
+
+    // Renderizar la nota
     mostrarNotas();
 }
 
 function clickBtnAgregar() {
+    // Traemos txtNota
     var txtNota = document.getElementById("txtNota");
 
+    // Queda asignado al localStorage
     localStorage.nota = txtNota.value;
     mostrarNotas();
 }
 
 function mostrarNotas() {
+    // Trae el Div Notas
     var divNotas = document.getElementById("divNotas");
+    // Le asigna las notas desde el LocalStorage
     divNotas.innerHTML = localStorage.nota;
 }
